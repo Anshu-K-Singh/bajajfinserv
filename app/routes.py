6b89d2cc88b070b0025352cfd0621a816786aa77
Anshu-K-Singh <anshusingh9907@gmail.com>
2024-11-21 17:20:27 +0530

    deploy on github

--- app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-bp = Blueprint('main', __name__)
-
-@bp.route('/bfhl', methods=['GET'])
-def get_operation_code():
-    return jsonify({"operation_code": 1}), 200
